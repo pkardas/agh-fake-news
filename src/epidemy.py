43dbf6e72f98e555b7547b07df41b4ec8a26cee4
Piotr Kardaś <pkardas.it@gmail.com>
2020-12-19 11:36:29 +0100

    Added tweets graph

--- src/epidemy.py
+++ src/epidemy.py
@@ -1,19 +1,20 @@
 import logging
 
 from src.data import get_tweets
-from src.tweets import plot_all_tweets_propagation
+from src.tweets import plot_tweet_relations, TweetType, plot_all_tweets_propagation
 
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
 
 tweets = get_tweets()
 
-# true_tweets = [tweet for tweet in tweets if tweet.tweet_type is TweetType.TRUE]
-# false_tweets = [tweet for tweet in tweets if tweet.tweet_type is TweetType.FALSE]
-# non_rumor_tweets = [tweet for tweet in tweets if tweet.tweet_type is TweetType.NON_RUMOR]
-# unverified_tweets = [tweet for tweet in tweets if tweet.tweet_type is TweetType.UNVERIFIED]
-#
+true_tweets = [tweet for tweet in tweets if tweet.tweet_type is TweetType.TRUE]
+false_tweets = [tweet for tweet in tweets if tweet.tweet_type is TweetType.FALSE]
+
 # plot_single_tweet_propagation(true_tweets[0])
 # plot_single_tweet_propagation(false_tweets[0])
 
 plot_all_tweets_propagation(tweets)
+plot_tweet_relations(false_tweets[1])
+plot_tweet_relations(true_tweets[2])
+
--- src/tweets.py
+++ src/tweets.py
@@ -3,11 +3,15 @@ from __future__ import annotations
 from collections import defaultdict
 from dataclasses import dataclass
 from enum import Enum
+from math import sqrt
 from typing import List, Optional
 
+import networkx
+from networkx import Graph
 from plotly.graph_objs import Scatter, Figure, Layout
 from plotly.graph_objs.layout import YAxis, XAxis, Title
 from plotly.graph_objs.scatter import Marker
+import matplotlib.pyplot as plt
 
 
 class TweetType(str, Enum):
@@ -24,9 +28,11 @@ class Tweet:
     content: str
     tweet_type: TweetType
     children: List[Tweet]
+    parent_unique_id: Optional[int] = None
 
     def insert_children(self, parent_unique_id: int, children: Tweet) -> bool:
         if self.unique_id == parent_unique_id:
+            children.parent_unique_id = parent_unique_id
             self.children.append(children)
             return True
 
@@ -81,6 +87,45 @@ def plot_all_tweets_propagation(all_tweets: List[Tweet]) -> None:
             xaxis=XAxis(title="Delay since root tweet"),
         )
     ).show()
+
+
+def plot_tweet_relations(tweet: Tweet) -> None:
+    tree = get_tweet_graph(tweet)
+
+    pos = networkx.spring_layout(tree)
+
+    plt.figure(figsize=(16, 16))
+    plt.title(f"{tweet.tweet_type}: '{tweet.content}'")
+
+    networkx.draw_networkx_nodes(
+        tree, pos,
+        nodelist=tree.nodes,
+        node_color='b',
+        node_size=100,
+        alpha=0.8
+    )
+    networkx.draw_networkx_nodes(
+        tree, pos,
+        nodelist=[tweet.unique_id],
+        node_color='g' if tweet.tweet_type is TweetType.TRUE else 'r',
+        node_size=500,
+        alpha=0.8
+    )
+    networkx.draw_networkx_edges(tree, pos, width=1.0, alpha=0.5)
+
+    plt.show()
+
+
+def get_tweet_graph(tweet: Tweet) -> Graph:
+    g = Graph()
+
+    for current_tweet in [tweet] + tweet.all_children:
+        if not current_tweet.parent_unique_id:
+            g.add_node(current_tweet.unique_id)  # root
+            continue
+        g.add_edge(current_tweet.parent_unique_id, current_tweet.unique_id)
+
+    return g
 
 
 def _get_single_tweet_scatter(tweet: Tweet, color: Optional[str] = None) -> Scatter:
