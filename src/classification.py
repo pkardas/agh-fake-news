97c68eb109b196a4985b870cea04d3e2413f113e
Piotr Kardaś <pkardas.it@gmail.com>
2020-12-28 12:57:38 +0100

    Added classifier_analysis and VotingClassifiers

--- src/classification.py
+++ src/classification.py
@@ -1,10 +1,9 @@
 import logging
 
 from sklearn.ensemble import (
-    RandomForestClassifier, AdaBoostClassifier,
-    GradientBoostingClassifier, ExtraTreesClassifier,
+    RandomForestClassifier, GradientBoostingClassifier, ExtraTreesClassifier, VotingClassifier,
 )
-from sklearn.linear_model import LogisticRegression, PassiveAggressiveClassifier, SGDClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score, roc_auc_score, accuracy_score, recall_score, precision_score
 from sklearn.model_selection import train_test_split
 from tabulate import tabulate
@@ -16,7 +15,7 @@ from src.utils import setup
 logger = logging.getLogger()
 
 
-def test_classifier(classifier, x_train, x_test, y_train, y_test):
+def check_classifier(classifier, x_train, x_test, y_train, y_test):
     classifier.fit(x_train, y_train)
     y_pred = classifier.predict(x_test)
 
@@ -42,13 +41,30 @@ def main():
     data = train_test_split(transformed_news, labels, shuffle=True, random_state=42)
 
     # Test various classifiers
-    test_classifier(LogisticRegression(max_iter=1000), *data)
-    test_classifier(PassiveAggressiveClassifier(), *data)
-    test_classifier(SGDClassifier(), *data)
-    test_classifier(RandomForestClassifier(), *data)
-    test_classifier(AdaBoostClassifier(), *data)
-    test_classifier(GradientBoostingClassifier(), *data)
-    test_classifier(ExtraTreesClassifier(), *data)
+    check_classifier(LogisticRegression(max_iter=1000), *data)
+    check_classifier(RandomForestClassifier(), *data)
+    check_classifier(GradientBoostingClassifier(), *data)
+    check_classifier(ExtraTreesClassifier(), *data)
+
+    check_classifier(VotingClassifier(
+        estimators=[
+            ("lr", LogisticRegression(max_iter=1000)),
+            ("rf", RandomForestClassifier()),
+            ("gd", GradientBoostingClassifier()),
+            ("et", ExtraTreesClassifier())
+        ],
+        voting="hard"
+    ), *data)
+
+    check_classifier(VotingClassifier(
+        estimators=[
+            ("lr", LogisticRegression(max_iter=1000)),
+            ("rf", RandomForestClassifier()),
+            ("gd", GradientBoostingClassifier()),
+            ("et", ExtraTreesClassifier())
+        ],
+        voting="soft"
+    ), *data)
 
 
 main()
