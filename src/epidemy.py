968ea700fb711b7f95c32256d3bf139c6ecfaada
Piotr Kardaś <pkardas.it@gmail.com>
2020-12-28 15:33:33 +0100

    Measured R0

--- src/epidemy.py
+++ src/epidemy.py
@@ -1,12 +1,16 @@
 import logging
 
+import numpy as np
+from scipy.stats import hmean
+from tabulate import tabulate
+
 from src.data import get_tweets
-from src.tweets import plot_tweet_relations, TweetType, plot_all_tweets_propagation
+from src.tweets import TweetType
 
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
 
-tweets = get_tweets()
+tweets = get_tweets(use_local=True)
 
 true_tweets = [tweet for tweet in tweets if tweet.tweet_type is TweetType.TRUE]
 false_tweets = [tweet for tweet in tweets if tweet.tweet_type is TweetType.FALSE]
@@ -14,7 +18,48 @@ false_tweets = [tweet for tweet in tweets if tweet.tweet_type is TweetType.FALSE
 # plot_single_tweet_propagation(true_tweets[0])
 # plot_single_tweet_propagation(false_tweets[0])
 
-plot_all_tweets_propagation(tweets)
-plot_tweet_relations(false_tweets[1])
-plot_tweet_relations(true_tweets[2])
+# plot_all_tweets_propagation(tweets)
+# false_tweets[1].plot_relations()
+# true_tweets[2].plot_relations()
 
+true_repr_nums = [true_tweet.get_reproduction_num(False) for true_tweet in true_tweets]
+true_repr_nums_all = [true_tweet.get_reproduction_num(True) for true_tweet in true_tweets]
+
+false_repr_nums = [false_tweet.get_reproduction_num(False) for false_tweet in false_tweets]
+false_repr_nums_all = [false_tweet.get_reproduction_num(True) for false_tweet in false_tweets]
+
+columns = [
+    "True/False",
+    "only_spreaders: Mean", "only_spreaders: HMean", "only_spreaders: Min", "only_spreaders: Max"
+]
+
+table_data = [
+    [
+        "true_tweets",
+        np.average(true_repr_nums), hmean(true_repr_nums), np.min(true_repr_nums), np.max(true_repr_nums),
+    ],
+    [
+        "false_tweets",
+        np.average(false_repr_nums), hmean(false_repr_nums), np.min(false_repr_nums), np.max(false_repr_nums),
+    ],
+]
+print(tabulate(table_data, headers=columns))
+
+columns = [
+    "True/False",
+    "all: Mean", "all: HMean", "all: Min", "all: Max"
+]
+
+table_data = [
+    [
+        "true_tweets",
+        np.average(true_repr_nums_all), hmean(true_repr_nums_all),
+        np.min(true_repr_nums_all), np.max(true_repr_nums_all),
+    ],
+    [
+        "false_tweets",
+        np.average(false_repr_nums_all), hmean(false_repr_nums_all),
+        np.min(false_repr_nums_all), np.max(false_repr_nums_all),
+    ],
+]
+print(tabulate(table_data, headers=columns))
--- src/tweets.py
+++ src/tweets.py
@@ -3,15 +3,15 @@ from __future__ import annotations
 from collections import defaultdict
 from dataclasses import dataclass
 from enum import Enum
-from math import sqrt
 from typing import List, Optional
 
+import matplotlib.pyplot as plt
 import networkx
+import numpy as np
 from networkx import Graph
 from plotly.graph_objs import Scatter, Figure, Layout
 from plotly.graph_objs.layout import YAxis, XAxis, Title
 from plotly.graph_objs.scatter import Marker
-import matplotlib.pyplot as plt
 
 
 class TweetType(str, Enum):
@@ -53,17 +53,81 @@ class Tweet:
 
         return self.children + all_children
 
+    def get_reproduction_num(self, include_non_spreaders: bool = True) -> float:
+        root_spreaders = defaultdict(list)
 
-def plot_single_tweet_propagation(tweet: Tweet) -> None:
-    Figure(
-        data=[_get_single_tweet_scatter(tweet)],
-        layout=Layout(
-            template="plotly_white",
-            title=Title(text=f"Tweets propagation, root: {tweet.unique_id}, {tweet.tweet_type}", x=0.5),
-            yaxis=YAxis(title="Cumulative number of retweets"),
-            xaxis=XAxis(title="Delay since root tweet"),
+        for tweet in self.all_children:
+            root_spreaders[tweet.parent_unique_id].append(tweet)
+
+        num_of_non_spreaders = len(self.all_children) + 1 - len(root_spreaders)
+
+        return np.average(
+            [len(infected) for infected in root_spreaders.values()] +
+            [0] * (num_of_non_spreaders if include_non_spreaders else 0)
         )
-    ).show()
+
+    def get_single_propagation_scatter(self, color: Optional[str] = None) -> Scatter:
+        time = defaultdict(int)
+
+        for child in self.all_children:
+            time[child.delay] += 1
+
+        x = sorted(time.keys())
+        y = []
+
+        for delay in x:
+            prev_sum = y[-1] if y else 0
+            y.append(prev_sum + time[delay])
+
+        return Scatter(x=x, y=y, marker=Marker(color=color) if color else None, text=self.content)
+
+    def plot_propagation(self) -> None:
+        Figure(
+            data=[self.get_single_propagation_scatter()],
+            layout=Layout(
+                template="plotly_white",
+                title=Title(text=f"Tweets propagation, root: {self.unique_id}, {self.tweet_type}", x=0.5),
+                yaxis=YAxis(title="Cumulative number of retweets"),
+                xaxis=XAxis(title="Delay since root tweet"),
+            )
+        ).show()
+
+    def plot_relations(self) -> None:
+        tree = self._get_graph()
+
+        pos = networkx.spring_layout(tree)
+
+        plt.figure(figsize=(16, 16))
+        plt.title(f"{self.tweet_type}: '{self.content}'")
+
+        networkx.draw_networkx_nodes(
+            tree, pos,
+            nodelist=tree.nodes,
+            node_color='b',
+            node_size=100,
+            alpha=0.8
+        )
+        networkx.draw_networkx_nodes(
+            tree, pos,
+            nodelist=[self.unique_id],
+            node_color='g' if self.tweet_type is TweetType.TRUE else 'r',
+            node_size=500,
+            alpha=0.8
+        )
+        networkx.draw_networkx_edges(tree, pos, width=1.0, alpha=0.5)
+
+        plt.show()
+
+    def _get_graph(self) -> Graph:
+        g = Graph()
+
+        for current_tweet in [self] + self.all_children:
+            if not current_tweet.parent_unique_id:
+                g.add_node(current_tweet.unique_id)  # root
+                continue
+            g.add_edge(current_tweet.parent_unique_id, current_tweet.unique_id)
+
+        return g
 
 
 def plot_all_tweets_propagation(all_tweets: List[Tweet]) -> None:
@@ -76,7 +140,7 @@ def plot_all_tweets_propagation(all_tweets: List[Tweet]) -> None:
 
     Figure(
         data=[
-            _get_single_tweet_scatter(tweet, colors[tweet.tweet_type])
+            tweet.get_single_propagation_scatter(colors[tweet.tweet_type])
             for tweet in all_tweets
             if tweet.tweet_type in {TweetType.TRUE, TweetType.FALSE}
         ],
@@ -87,58 +151,3 @@ def plot_all_tweets_propagation(all_tweets: List[Tweet]) -> None:
             xaxis=XAxis(title="Delay since root tweet"),
         )
     ).show()
-
-
-def plot_tweet_relations(tweet: Tweet) -> None:
-    tree = get_tweet_graph(tweet)
-
-    pos = networkx.spring_layout(tree)
-
-    plt.figure(figsize=(16, 16))
-    plt.title(f"{tweet.tweet_type}: '{tweet.content}'")
-
-    networkx.draw_networkx_nodes(
-        tree, pos,
-        nodelist=tree.nodes,
-        node_color='b',
-        node_size=100,
-        alpha=0.8
-    )
-    networkx.draw_networkx_nodes(
-        tree, pos,
-        nodelist=[tweet.unique_id],
-        node_color='g' if tweet.tweet_type is TweetType.TRUE else 'r',
-        node_size=500,
-        alpha=0.8
-    )
-    networkx.draw_networkx_edges(tree, pos, width=1.0, alpha=0.5)
-
-    plt.show()
-
-
-def get_tweet_graph(tweet: Tweet) -> Graph:
-    g = Graph()
-
-    for current_tweet in [tweet] + tweet.all_children:
-        if not current_tweet.parent_unique_id:
-            g.add_node(current_tweet.unique_id)  # root
-            continue
-        g.add_edge(current_tweet.parent_unique_id, current_tweet.unique_id)
-
-    return g
-
-
-def _get_single_tweet_scatter(tweet: Tweet, color: Optional[str] = None) -> Scatter:
-    time = defaultdict(int)
-
-    for child in tweet.all_children:
-        time[child.delay] += 1
-
-    x = sorted(time.keys())
-    y = []
-
-    for delay in x:
-        prev_sum = y[-1] if y else 0
-        y.append(prev_sum + time[delay])
-
-    return Scatter(x=x, y=y, marker=Marker(color=color) if color else None, text=tweet.content)
